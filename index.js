2cfb7c9b2f4bf759f4285fc205294be351123736
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-09-04 18:21:34 +0000

    node3 updates.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-        const server = require('./api/server.js');
+        const server = require('Server.js');
         
         const port = 8000;
         server.listen(port, () => console.log(`Server listening on port ${port}`))
